725f0fe3eeecb7fc701281328f4dcf905cd8f1b8
Shinichi <s1930557@s.tsukuba.ac.jp>
2021-09-08 11:18:55 +0900

    make test analysis script

--- tests/Image_subtraction_analysis.py
+++ tests/Image_subtraction_analysis.py
@@ -21,11 +21,11 @@ from skimage.color import rgb2gray
 from skimage import io
 from skimage import io, img_as_float, img_as_ubyte
 from tqdm import tqdm
-from skimage.filters.rank import median
+from skimage.filters import median, gaussian
 from skimage.morphology import disk
 
 # parameters
-threshold = 5
+threshold = 7
 
 
 def get_image_filelist():
@@ -50,46 +50,24 @@ def get_image_filelist():
 def data_select():
     file_list: list[str] = get_image_filelist()
     number_of_images: int = len(file_list) - 1
-    if number_of_images < 100:
-        messagebox.showinfo('cancel', 'there are not enough amount of images')
-    else:
-        pass
     return file_list, number_of_images
 
 
-def threshold_set(file_list, number_of_images):
-    sampling_cycle = (number_of_images // 100) - 1
-    threshold_list = []
-    for p in tqdm(range(100)):
-        sample_num = p * sampling_cycle
-        sample_path = file_list[sample_num]
-        next_to_sample_path = file_list[sample_num + 1]
-        sample_img = rgb2gray(img_as_float(io.imread(sample_path)))
-        sample_img2 = rgb2gray(img_as_float(io.imread(next_to_sample_path)))
-        if p == 0:
-            sample_subtract_img = cv2.absdiff(sample_img, sample_img2)
-        else:
-            sub = cv2.absdiff(sample_img, sample_img2)
-            sample_subtract_img = np.vstack([sample_subtract_img, sub])
-    sample_mean = sample_subtract_img.mean()
-    sample_std = sample_subtract_img.std()
-    threshold_pixel = threshold * sample_std + sample_mean
-
-    return threshold_pixel
-
-
-def image_subtraction_analysis(file_list, threshold_pixel):
+def image_subtraction_analysis(file_list):
     t1 = time.time()
+    threshold_list = []
     data_list: list = []
     for n in tqdm(range(len(file_list) - 1)):
         img1_path = file_list[n]
         img2_path = file_list[(int(n) + 1)]
-
-        img1: ndarray = rgb2gray(img_as_float(io.imread(img1_path)))
-        img2: ndarray = rgb2gray(img_as_float(io.imread(img2_path)))
+        img1: ndarray = cv2.GaussianBlur(rgb2gray(img_as_float(io.imread(img1_path))), (5,5), 1)
+        img2: ndarray = cv2.GaussianBlur(rgb2gray(img_as_float(io.imread(img2_path))), (5,5), 1)
         subtracted_image = cv2.absdiff(img1,img2)
-        data_list.append(np.count_nonzero(subtracted_image > threshold_pixel))
-    data_array = np.array(data_list)
+        subtracted_image = cv2.GaussianBlur(subtracted_image, (5,5), 1)
+        threshold = subtracted_image.mean() + 7 * subtracted_image.std()
+        data_list.append(np.count_nonzero(subtracted_image > threshold))
+        threshold_list.append(threshold)
+    data_array = np.array([data_list, threshold_list])
     t2 = time.time()
     elapsed_time = t2 - t1
     print(f"経過時間：{elapsed_time}")
@@ -97,7 +75,7 @@ def image_subtraction_analysis(file_list, threshold_pixel):
 
 
 def data_save(data_list, elapsed_time):
-    s = pd.DataFrame(data_list)
+    s = pd.DataFrame(data_list.T)
     s_name = os.path.basename("./")
 
     date = datetime.date.today()
@@ -117,8 +95,7 @@ def data_save(data_list, elapsed_time):
 
 def main():
     file_list, number_of_images = data_select()
-    threshold_pixel = threshold_set(file_list, number_of_images)
-    data_array, elapsed_time = image_subtraction_analysis(file_list, threshold_pixel)
+    data_array, elapsed_time = image_subtraction_analysis(file_list)
     data_save(data_array, elapsed_time)
     sys.exit()
 

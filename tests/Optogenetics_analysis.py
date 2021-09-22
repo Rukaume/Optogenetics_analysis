03594a87166e40d039d2367800cde5f1f6b4d666
Shinichi <s1930557@s.tsukuba.ac.jp>
2021-09-22 16:04:29 +0900

    add

--- tests/Optogenetics_analysis.py
+++ tests/Optogenetics_analysis.py
@@ -6,7 +6,7 @@ import tkinter
 from tkinter import messagebox
 from tkinter import filedialog
 
-stimuli_list =[1700, 2147, 2567, 3217, 3614]
+stimuli_list =[1420, 2128, 2514, 3097, 3475]
 
 threshold = 50
 Shortest_M_bout_duration = 5

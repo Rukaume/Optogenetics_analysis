bb056ed2cbd689c10d754a82e09e1590fa421197
Shinichi <s1930557@s.tsukuba.ac.jp>
2021-09-29 11:22:37 +0900

    complete the extraction script

--- tests/data_extract_ALA_activity.py
+++ tests/data_extract_ALA_activity.py
@@ -47,10 +47,10 @@ def search_subdivided_dir_path():
                 stimuli_data_list.append(os.path.join(curDir, file))
             else:
                 pass
-    return stimuli_data_list, date_list
+    return stimuli_data_list
 
 
-def data_extraction_and_summarize(stimuli_data_list, date_list):
+def data_extraction_and_summarize(stimuli_data_list):
     data_dict = {}
     for path in stimuli_data_list:
         # get date
@@ -60,38 +60,44 @@ def data_extraction_and_summarize(stimuli_data_list, date_list):
         else:
             pass
         # get experiment name
-        current_data_experiment_name_list = [i for i in path.split("\\") if re.match("\d+_remEx\.+", i)]
+        current_data_experiment_name_list = [i for i in path.split("\\") if re.findall("\w+_remEx\w+\+|\-+\w+", i)]
         if current_data_experiment_name_list:
-            current_data_experiment_name = current_data_experiment_name_list[0]
+            current_data_experiment_name = current_data_experiment_name_list[0].split("/")[-1]
+        else:
+            pass
+        # get worm number
+        current_worm_number_list = [i for i in path.split("\\") if re.findall("_\d", i)]
+        if current_worm_number_list:
+            current_worm_number = "worm" + current_worm_number_list[0]
         else:
             pass
         # get stimuli number
-        current_stimuli_number = path.split("\\")[-1].split("_")[-1]
+        current_stimuli_number = path.split("\\")[-1].split("_")[-1].split(".")[0]
 
         # make destination directory
-        os.makedirs("./extracted_data/{}".format(current_data_analyzed_date), exist_ok=True)
+        os.makedirs("./extracted_data", exist_ok=True)
 
         # get data
         current_data_array = pd.read_csv(path).values
-        current_data_name = current_data_experiment_name + "Stimuli_" + current_stimuli_number
+        current_data_name = current_data_experiment_name + current_worm_number + "Stimuli_" + current_stimuli_number
 
         # add data
         if current_data_analyzed_date in data_dict:
             saved_df = data_dict[current_data_analyzed_date]
             saved_df[current_data_name] = current_data_array
         else:
-            data_dict[current_data_analyzed_date] = pd.DataFrame(current_data_array,
-                                                                 columns=current_data_name)
+            data_dict[current_data_analyzed_date] = pd.DataFrame(current_data_array.T[0],
+                                                                 columns = [current_data_name])
 
     for key in data_dict:
         data = data_dict[key]
-        data.to_csv("./extracted_data/{}".format(key))
+        data.to_csv("./extracted_data/{}.csv".format(key))
 
 
 def main():
     ask_dir_name()
-    stimuli_data_list, date_list = search_subdivided_dir_path()
-    data_extraction_and_summarize(stimuli_data_list, date_list)
+    stimuli_data_list = search_subdivided_dir_path()
+    data_extraction_and_summarize(stimuli_data_list)
 
 
 if __name__ == '__main__':
